4dbc4fc1daf15be7fd9c214c103efec1ca5fdd86
MahendraDwivedi <dwivedimahendra9876@gmail.com>
2025-09-24 22:47:03 +0530

    h1 added to show server ins running

--- server/app.js
+++ server/app.js
@@ -19,8 +19,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
 app.use('/api/tenants', tenantsRoutes);
 
+// server/app.js
+app.get('/', (req, res) => {
+  res.send('<h1>Server is running ✅</h1><p>Welcome to the backend!</p>');
+});
+
 app.use('/api/*', (req, res) => {
   res.status(404).json({ error: 'Not found' });
 });
+
+
 
 module.exports = app;
